b3a50976cade6bdaf17cec15ca5e0f0b01b17284
Mariusz 'Mario' Zajac <zajac@gmx.us>
2021-12-10 09:39:03 +0100

    Add new class

---
+++ ACM.BL/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+        
+    }
+}
---
+++ ACM.BL/Product.cs
@@ -0,0 +1,37 @@
+﻿namespace ACM.BL
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+    public decimal? CurrentPrice { get; set; }
+    public string ProductDescription { get; set; }
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; }
+
+    public Product Retrieve(int productId)
+    {
+        return new Product();
+    }
+
+    public bool Save()
+    {
+        return true;
+    }
+
+    public bool Validate()
+    {
+        bool isValid = !string.IsNullOrWhiteSpace(ProductName);
+        if (CurrentPrice == null) isValid = false;
+        return isValid;
+    }
+    
+    }
+}
