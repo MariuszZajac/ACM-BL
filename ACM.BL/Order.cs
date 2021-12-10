ff99ba50680c7e42fc21210c1df33abf9d335626
Mariusz 'Mario' Zajac <zajac@gmx.us>
2021-12-10 10:06:10 +0100

    Nev method

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -15,6 +15,20 @@ namespace ACM.BL
 
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
-        
+
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            bool isValid = !(OrderDate==null);
+            return isValid;
+        }
     }
 }
